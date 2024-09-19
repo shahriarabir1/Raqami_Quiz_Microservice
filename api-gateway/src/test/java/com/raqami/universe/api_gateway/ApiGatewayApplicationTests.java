016eb380472dc7e94bcc011f02c40af03e0d61dd
Shahriar Abir <shahriarabirjob@gmail.com>
2024-09-20 01:43:48 +0600

    version-1.0.1

---
+++ api-gateway/src/test/java/com/raqami/universe/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raqami.universe.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
